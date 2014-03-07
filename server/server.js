faef586b9fd824d50591bf5e80feb09fc05185ba
lheilman <lheilman@purdue.edu>
2014-03-07 16:41:24 -0500

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -5,12 +5,7 @@ Meteor.publish("directory", function() {
 		});
 
 Meteor.publish("current_events", function() {
-<<<<<<< HEAD
 	return CurrentEvents.find({});
 });
 
 
-=======
-		return CurrentEvents.find({});
-		});
->>>>>>> c6e60328c7e3ee7dbe307e5903a090bc7e24c820

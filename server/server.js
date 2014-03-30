dac169c37ddb61fa6891e41cf7188b689af36f95
sakshay2304 <sakshay2304@gmail.com>
2014-03-30 18:13:03 -0400

    merge facebook info

--- server/server.js
+++ server/server.js
@@ -1,5 +1,41 @@
 // Say Hello - server
 
+
+/*
+	This uses the accessToken from the Facebook login widget to gain access 
+	to user profile
+*/
+Accounts.onCreateUser(function(options, user) {
+    if (options.profile) { // maintain the default behavior
+        user.profile = options.profile;
+    }
+
+    // get profile data from Facebook
+    var result = Meteor.http.get("https://graph.facebook.com/me", {
+      params: {access_token: user.services.facebook.accessToken}});
+
+    if ( !result.error && result.data ) {
+        // if successfully obtained facebook profile, save it off
+        user.profile.facebook = result.data;
+    }
+
+
+
+    return user;
+});
+
+/*
+	The information grabbed from Facebook cannot be accessed by the client
+	unless the server publishes the information
+*/
+Meteor.publish("facebook_info", function() { 
+	return Meteor.users.find({_id: this.userId}, {fields: {'services.facebook.email': 1,
+								'services.facebook.gender': 1,
+								'services.facebook.locale': 1,
+								'services.facebook.id': 1}}); 
+});
+
+
 Meteor.publish("directory", function() {
 		return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
 		});

839b8c0f07d879a44859bb9fe04d8ad2f16dcfd9
Soumya Kumar <soumyakumar85@gmail.com>
2017-02-12 23:33:52 -0500

    Some file cleanup

--- client/client.js
+++ client/client.js
@@ -12,7 +12,19 @@ Meteor.startup(function() {
     }
   });
 });
+Meteor.subscribe("friends");
 
+Template.account_tab.friendListFinal = function() {
+   myTest = new Array();
+    myTest[0] = "Help";
+    return myTest;
+}
+
+
+	var alti = new Array();
+	alti[0] = "No Account"
+	alti[1] = "Login!"
+	return alti;
 if(Meteor.isClient) {
   console.log("Welcome to client");
   Meteor.subscribe("friends");
